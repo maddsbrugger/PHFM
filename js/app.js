34eb1f33d801c5bb88447d6ffbb422e9561b0705
Maddsbrugger <hannahjaneblair@gmail.com>
2017-03-31 03:18:15 -0400

    added mobile menu and updated css and added more html pages

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//mobile menu adapted from Andrew Chalkley's "Creating a Mobile Drop Down Menu" lesson on Treehouse
+
+
+
+//Create a select and append to #menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+
+//Cycle over menu links
+$("#menu a").each(function(){
+  var $anchor = $(this);
+  //Create an option
+  var $option = $("<option></option>");
+
+  //Deal with selected options depending on current page
+  if($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
+  //option's value is the href
+  $option.val($anchor.attr("href"));
+  //option's text is the text of link
+  $option.text($anchor.text());
+  //append option to select
+  $select.append($option);
+});
+
+//Bind change listener to the select
+$select.change(function(){
+  //Go to select's location
+  window.location = $select.val();
+});
